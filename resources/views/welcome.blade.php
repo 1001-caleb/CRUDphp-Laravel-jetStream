dde9a85d740534a82f15a589700e70f867ea809a
Caleb Parrra <mccalebyt@gmail.com>
2022-04-26 22:27:41 -0500

    cositas

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -11,7 +11,7 @@
     <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
-
+    <!-- <script src="https://cdn.tailwindcss.com"></script> -->
     <style>
         .bg-naranja {
             background-color: rgb(249 115 22);
@@ -20,10 +20,13 @@
             display: grid;
             grid-template-columns: repeat(2, 1fr);
             place-items: center;
+            padding: 1.25rem;
+            
         }
         @media screen and (max-width:425px) {
             .contain{
                 grid-template-columns: repeat(1, 1fr);
+                gap: 5rem;
             }
         }
         body {
@@ -61,11 +64,11 @@
         </div>   
     </nav>
 
-    <section class="contain p-5">
+    <section class="contain">
         <div>
             <h1 class="text-white">Aplicativo CNCA</h1>
             <p class="text-white">Con este aplicativo se podrá organizar información acerca de los instructores del centro nacional colombo aleman</p>
-            <a href="{{ url('/articulos') }}" class="button text-decoration-none p-2 bg-naranja border text-white rounded-3">Entrar</a>
+            <a href="{{ url('/articulos ') }}" class="button text-decoration-none p-2 bg-naranja border text-white rounded-3">Entrar</a>
         </div>
         <div class="w-1">
             <img class="img-fluid" src="{{URL::asset('assets/teach.svg')}}" type="image/svg" alt="img">
